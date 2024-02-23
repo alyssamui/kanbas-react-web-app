f43d3f59897e8048d570b16966fc803839e50728
Alyssa Mui <alyssamui23@gmail.com>
2024-02-23 01:45:31 -0500

    progress

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,22 @@ import logo from "./logo.svg";
 import "./App.css";
 import Labs from "./Labs";
 import HelloWorld from "./Labs/a3/HelloWorld";
+import { HashRouter } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router";
 import Kanbas from "./Kanbas";
 
 function App() {
   return (
-    <div>
-      <Labs />
-      <Kanbas />
-      <HelloWorld />
-    </div>
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="/Labs" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
+          <Route path="/hello" element={<HelloWorld />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
---
+++ src/Kanbas/Navigation/index.tsx
@@ -0,0 +1,48 @@
+import { Link, useLocation } from "react-router-dom";
+import "./index.css";
+import {
+  FaTachometerAlt,
+  FaRegUserCircle,
+  FaBook,
+  FaRegCalendarAlt,
+  FaInbox,
+  FaClock,
+  FaTv,
+  FaArrowAltCircleRight,
+  FaQuestionCircle,
+} from "react-icons/fa";
+import { TbLetterN } from "react-icons/tb";
+function KanbasNavigation() {
+  const links = [
+    { label: "Account", icon: <FaRegUserCircle className="fs-2" id="acc" /> },
+    { label: "Dashboard", icon: <FaTachometerAlt className="fs-2" /> },
+    { label: "Courses", icon: <FaBook className="fs-2" /> },
+    { label: "Calendar", icon: <FaRegCalendarAlt className="fs-2" /> },
+    { label: "Inbox", icon: <FaInbox className="fs-2" /> },
+    { label: "History", icon: <FaClock className="fs-2" /> },
+    { label: "Studio", icon: <FaTv className="fs-2" /> },
+    { label: "Commons", icon: <FaArrowAltCircleRight className="fs-2" /> },
+    { label: "Help", icon: <FaQuestionCircle className="fs-2" /> },
+  ];
+  const { pathname } = useLocation();
+  return (
+    <ul className="wd-kanbas-navigation">
+      <div className="northeastern">
+        <Link to={"https://www.northeastern.edu/"}>N </Link>
+      </div>
+      {links.map((link, index) => (
+        <li
+          key={index}
+          className={pathname.includes(link.label) ? "wd-active" : ""}
+        >
+          <Link to={`/Kanbas/${link.label}`}>
+            {" "}
+            {link.icon} <br />
+            {link.label}{" "}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+export default KanbasNavigation;
