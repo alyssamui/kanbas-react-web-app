327c143ac85057a718ca784a478decfe48ccb771
Alyssa Mui <mui.a@northeastern.edu>
2024-02-22 23:59:46 -0500

    lab

---
+++ src/Labs/a3/JavaScript/arrays/WorkingWithArrays.tsx
@@ -0,0 +1,36 @@
+import FilterFunction from "./FilterFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+
+var functionScoped = 2;
+let blockScoped = 5;
+const constant1 = functionScoped - blockScoped;
+let numberArray1 = [1, 2, 3, 4, 5];
+let stringArray1 = ["string1", "string2"];
+
+let variableArray1 = [
+  functionScoped,
+  blockScoped,
+  constant1,
+  numberArray1,
+  stringArray1,
+];
+
+function WorkingWithArrays() {
+  return (
+    <>
+      <h3>Working with Arrays</h3>
+      numberArray1 = {numberArray1}
+      <br />
+      stringArray1 = {stringArray1}
+      <br />
+      variableArray1 = {variableArray1}
+      <br />
+      <FindFunction />
+      <FindIndex />
+      <FilterFunction />
+    </>
+  );
+}
+
+export default WorkingWithArrays;
